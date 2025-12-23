28495dfa33a2d5364da231bd9f38318b688ecfb1
Korchagin060912X <egorikoffice89@vk.com>
2025-12-23 10:00:25 +0200

    Delete culinary_blog-main/blog directory

--- culinary_blog-main/blog/recipes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'recipes'
-
-urlpatterns = [
-    path('', views.recipe_list, name='recipe_list'),
-    path('<slug:slug>/', views.recipe_detail, name='recipe_detail'),
-]
